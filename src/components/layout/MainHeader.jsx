a7346e68d99e4a2282747bb0ea9cf809d16961d8
NguyenVanAnit <ankato293@gmail.com>
2025-04-24 17:06:52 +0700

    edit ui anh install lib

--- src/components/layout/MainHeader.jsx
+++ src/components/layout/MainHeader.jsx
@@ -4,7 +4,7 @@ const MainHeader = () => {
             <div className="overlay"></div>
             <div className="animated-texts overlay-content">
                 <h1>
-                    Xin chào đến với <span className="hotel-color"> Khách sạn Bách Khoa</span>
+                    Xin chào đến với <span className="hotel-color"> Khách sạn An An</span>
                 </h1>
                 <h4>Nơi đem đến cho bạn khoảng thời gian tuyệt vời nhất</h4>
             </div>
