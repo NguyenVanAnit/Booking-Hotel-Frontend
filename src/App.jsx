425ae4302836de66e2115ec0f926cf678f6c30a7
NguyenVanAnit <ankato293@gmail.com>
2025-06-17 16:43:49 +0700

    add browser router

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
 import { ToastContainer } from "react-toastify";
 import ExistingRooms from "./components/room/ExistingRooms";
 import Home from "./components/home/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 import EditRoom from "./components/room/EditRoom";
 import NavBar from "./components/layout/NavBar";
 // import Footer from "./components/layout/Footer";
@@ -13,11 +19,11 @@ import Admin from "./components/admin/Admin";
 import Checkout from "./components/booking/Checkout";
 import BookingSuccess from "./components/booking/BookingSuccess";
 import Bookings from "./components/booking/BookingList";
-import Login from "./components/auth/Login"
-import Registration from "./components/auth/Registration"
-import Profile from "./components/auth/Profile"
-import { AuthProvider } from "./components/auth/AuthProvider"
-import RequireAuth from "./components/auth/RequireAuth"
+import Login from "./components/auth/Login";
+import Registration from "./components/auth/Registration";
+import Profile from "./components/auth/Profile";
+import { AuthProvider } from "./components/auth/AuthProvider";
+import RequireAuth from "./components/auth/RequireAuth";
 import ServiceList from "./components/service/ServiceList";
 import AddService from "./components/service/AddService";
 import ServiceOfRoom from "./components/room/ServicesOfRoom";
@@ -41,57 +47,108 @@ import StatisticRate from "./components/statistics/StatisticRate";
 import StatisticStaffAttendance from "./components/statistics/StatisticStaff";
 import CreateAccount from "./components/auth/CreateAccount";
 
+// import PropTypes from "prop-types";
+// ProtectedRoute.propTypes = {
+//   children: PropTypes.node.isRequired,
+//   allowedRoles: PropTypes.arrayOf(PropTypes.string).isRequired,
+// };
+
+const ProtectedRoute = ({ allowedRoles }) => {
+  const userRole = localStorage.getItem("userRole"); // ví dụ: 'ROLE_HR'
+
+  if (!userRole) return <Navigate to="/login" replace />;
+  if (!allowedRoles.includes(userRole))
+    return <Navigate to="/unauthorized" replace />;
+
+  return <Outlet />;
+};
+
+import PropTypes from "prop-types";
+ProtectedRoute.propTypes = {
+  allowedRoles: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
 function App() {
   return (
     <AuthProvider>
       <main>
-        <Router>
+        <BrowserRouter>
           <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/edit-room" element={<EditRoom />} />
-            <Route path="/add-room" element={<EditRoom />} />
-            <Route path="/existing-rooms" element={<ExistingRooms />} />
+            <Route path="*" element={<h1>404 Not Found</h1>} />
+            <Route
+              path="/unauthorized"
+              element={<h1>403 - Không đủ quyền</h1>}
+            />
             <Route path="/browse-all-rooms" element={<RoomListing />} />
-            <Route path="/admin" element={<Admin />} />
             <Route path="/detail-room" element={<DetailRoom />} />
-            <Route path="book-room" element={
-              <RequireAuth>
-                <Checkout />
-              </RequireAuth>
-            } />
-            <Route path="/booking-success" element={<BookingSuccess />} />
-            <Route path="/existing-bookings" element={<Bookings />} />
-
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Registration />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/service-list" element={<ServiceList />} />
-            <Route path="/add-service" element={<AddService />} />
-            <Route path="services-of-room" element={<ServiceOfRoom />} />
-            <Route path="/payment-result" element={<PaymentResult />} />
-            <Route path="/staff-list" element={<StaffList />} />
-            <Route path="/add-staff" element={<AddStaff />} />
-            <Route path="/time-keeping" element={<TimeKeeping />} />
-            <Route path="/statistics-home" element={<StatisticsHome />} />
-            <Route path="/history-booking" element={<HistoryBooking />} />
-            <Route path="/vnpay-return" element={<VNPayReturn />} />
-            <Route path="/assign-work" element={<AssignWork />} />
-            <Route path="/all-users" element={<UserList />} />
-            <Route path="/statistics-room" element={<StatisticRoom />} />
-            <Route path="/booking-detail" element={<BookingDetail />} />
-            <Route path="/search-booking" element={<SearchBooking />} />
-            <Route path="/assign-task" element={<AssignTaskPage />} />
-            <Route path="/room-task" element={<RoomTasksPage />} />
-            <Route path="/staff-task" element={<StaffTasksPage />} />
-            <Route path="/statistics-rate" element={<StatisticRate />} />
-            <Route path="/staff-statistics" element={<StatisticStaffAttendance />} />
-            <Route path="*" element={<h1>404 Not Found</h1>} />
-            <Route path="/create-account" element={<CreateAccount />} />
+
+            <Route element={<RequireAuth />}>
+              {/* admin */}
+              <Route element={<ProtectedRoute allowedRoles={["ROLE_ADMIN"]} />}>
+                <Route path="/edit-room" element={<EditRoom />} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/add-room" element={<EditRoom />} />
+                <Route path="/service-list" element={<ServiceList />} />
+                <Route path="/add-service" element={<AddService />} />
+                <Route path="services-of-room" element={<ServiceOfRoom />} />
+                <Route path="/statistics-home" element={<StatisticsHome />} />
+                <Route path="/all-users" element={<UserList />} />
+                <Route path="/create-account" element={<CreateAccount />} />
+                <Route path="/statistics-room" element={<StatisticRoom />} />
+                <Route path="/statistics-rate" element={<StatisticRate />} />
+                <Route
+                  path="/staff-statistics"
+                  element={<StatisticStaffAttendance />}
+                />
+              </Route>
+
+              {/* admin, hr */}
+              <Route
+                element={
+                  <ProtectedRoute allowedRoles={["ROLE_ADMIN", "ROLE_HR"]} />
+                }
+              >
+                <Route path="/existing-rooms" element={<ExistingRooms />} />
+                <Route path="/staff-list" element={<StaffList />} />
+                <Route path="/add-staff" element={<AddStaff />} />
+                <Route path="/time-keeping" element={<TimeKeeping />} />
+                <Route path="/assign-work" element={<AssignWork />} />
+                <Route path="/assign-task" element={<AssignTaskPage />} />
+                <Route path="/room-task" element={<RoomTasksPage />} />
+                <Route path="/staff-task" element={<StaffTasksPage />} />
+              </Route>
+
+              {/* login */}
+
+              <Route path="book-room" element={<Checkout />} />
+              <Route path="/booking-success" element={<BookingSuccess />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/payment-result" element={<PaymentResult />} />
+              <Route path="/vnpay-return" element={<VNPayReturn />} />
+              <Route path="/history-booking" element={<HistoryBooking />} />
+
+              {/* admin, receptionist */}
+
+              <Route
+                element={
+                  <ProtectedRoute
+                    allowedRoles={["ROLE_ADMIN", "ROLE_RECEPTIONIST"]}
+                  />
+                }
+              >
+                <Route path="/existing-bookings" element={<Bookings />} />
+                <Route path="/booking-detail" element={<BookingDetail />} />
+                <Route path="/search-booking" element={<SearchBooking />} />
+              </Route>
+            </Route>
           </Routes>
           {/* <Footer /> */}
-        </Router>
-        <ToastContainer/>
+        </BrowserRouter>
+        <ToastContainer />
       </main>
     </AuthProvider>
   );
--- src/components/auth/RequireAuth.jsx
+++ src/components/auth/RequireAuth.jsx
@@ -1,12 +1,9 @@
-import React from "react"
-import { Navigate, useLocation } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom";
 
-const RequireAuth = ({ children }) => {
-    const user = localStorage.getItem("userId")
-    const location = useLocation()
-    if (!user) {
-        return <Navigate to="/login" state={{ path: location.pathname }} />
-    }
-    return children
-}
-export default RequireAuth
+const RequireAuth = () => {
+  const user = localStorage.getItem("token");
+
+  return user ? <Outlet /> : <Navigate to="/login" replace />;
+};
+
+export default RequireAuth;
